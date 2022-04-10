6bb359dab39439beb69d0ebc3de15ef4f052d395
credo7 <ah.vit@mail.ru>
2022-04-10 16:01:16 +0300

    CountdownTimer added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import type { NextPage } from "next";
-import Todo from "../components/todo";
-import styles from '../styles/home.module.css';
+import CountdownTimer from "../components/CountdownTimer";
+import Todo from "../components/Todo";
+import styles from "../styles/home.module.css";
+import dayjs from "dayjs";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.main_container}>
       <Todo />
+      <CountdownTimer countdownTimestampMs={dayjs().year(2023).month(0).date(0).hour(0).minute(0).second(0)} />
     </div>
   );
 };
