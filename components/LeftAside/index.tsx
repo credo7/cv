975a8a5330245fda02f8d2a152433c207e8ae4e1
credo7 <ah.vit@mail.ru>
2022-04-14 17:20:13 +0300

    Skills added

--- components/LeftAside/index.tsx
+++ components/LeftAside/index.tsx
@@ -1,6 +1,38 @@
+import { useMemo } from "react";
 import style from "./leftAside.module.css";
 
+const skills = [
+  "Html",
+  "CSS",
+  "JS",
+  "ReactJS",
+  "Redux",
+  "NextJS",
+  "TS",
+  "Docker",
+  "Postgres",
+  "SQL",
+  "TypeORM",
+  "Prisma",
+  "Git",
+  "CMD",
+  "RESP API",
+  "NodeJS",
+  "NestJS",
+  "Express",
+  "Python",
+  "C",
+];
+
 const LeftAside = () => {
+  const skillsLi = useMemo(
+    () =>
+      skills.map((el, index) => {
+        return <div key={index}>{el}</div>;
+      }),
+    []
+  );
+
   return (
     <div className={style.container_aside}>
       <div className={style.img_container}>
@@ -11,9 +43,13 @@ const LeftAside = () => {
         <h2>Akhmetzyanov</h2>
       </div>
       <div className={style.media}>
-              <img src='whatsapp.svg'/>
-              <img src="telegram.svg" />
-              <img src="github.svg" />
+        <img src="whatsapp.svg" />
+        <img src="telegram.svg" />
+        <img src="github.svg" />
+      </div>
+      <div className={style.skills_container}>
+        <h2>Skills:</h2>
+        <div className={style.skills}>{skillsLi}</div>
       </div>
     </div>
   );
